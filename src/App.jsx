e3b2057e3c469b37264630c68dad1d49140825a6
Buppi Khan <khanbuppi@gmail.com>
2023-12-02 18:39:37 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      <h1>3D developer portfolio</h1>
+    </div>
+  );
+};
+
+export default App;
