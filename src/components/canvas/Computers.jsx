88d953c0fd2982a62ebc98800102eb0e9e59f8a6
Buppi Khan <khanbuppi@gmail.com>
2023-12-03 11:45:31 -0500

    implemented add section

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -52,6 +52,7 @@ const ComputersCanvas = () => {
     <Canvas
       frameloop="demand"
       shadows
+      dpr={[1, 2]}
       camera={{ position: [20, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
     >
